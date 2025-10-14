1f84c94ba6b21a34299abe071fa11193e9ebb162
kkoomin <kkooming2@gmail.com>
2025-10-14 17:41:47 +0900

    feat: 미사용 코드 및 에셋 삭제. portfolio 루트 생성

---
+++ app/portfolio/page.tsx
@@ -0,0 +1,11 @@
+export default function Page() {
+  return (
+    <div className="min-h-screen bg-slate-100 py-8 print:bg-white print:py-0">
+      {/* Paper-like container */}
+      <div className="max-w-5xl mx-auto bg-white shadow-lg print:shadow-none px-12 py-10">
+        portfolio portfolio portfolio portfolio portfolio portfolio portfolio
+        portfolio portfolio portfolio
+      </div>
+    </div>
+  );
+}
